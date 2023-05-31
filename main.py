4cd961ce28b5478ba734f504a589a7b772fe1543
s24260 <s24260@pjwstk.edu.pl>
2023-05-31 20:14:11 +0200

    Dodanie pobierania tekstu i zapisu do nowego pliku z kluczem

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import os
 from huffman import Huffman
 
-sentence = "test ale to nie jest test"
+file_name = "julian_tuwim_abecadlo.txt"
+new_file_name = "encoded_text.txt"
+# get resource from file
+file = open(file_name, "r")
+sentence = file.read()
+file.close()
 
+file_stats = os.stat(file_name)
+print(f'File Size in Bytes is {file_stats.st_size}')
+
+# huffman coding
 tree = Huffman.create_tree(sentence)
 code_array = Huffman.create_code_array(tree)
 encoded_text = Huffman.encode_text(sentence, code_array)
 decoded_text = Huffman.decode_text(encoded_text, tree)
 
-print(code_array)
-print(encoded_text)
-print(decoded_text)
+# create new file
+new_file = open(new_file_name, "a")
+new_file.write(f'{code_array}')
+new_file.write('\n')
+new_file.write(encoded_text)
+new_file.close()
+
+new_file_stats = os.stat(new_file_name)
+print(f'File Size in Bytes is {new_file_stats.st_size}')
